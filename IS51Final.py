b96fdb7af233cdd06e27ea26bdf230083684fb40
mohammed talib <m.t.talib@hotmail.com>
2022-05-19 16:27:03 -0700

    finished structured-english

---
+++ IS51Final.py
@@ -0,0 +1,12 @@
+"""
+Program opens and uses a file (final.txt) that has the grades for the students.
+we append the lines from the txt file to our program
+calculates count of grades
+calculate average by the sum of values divided by count of grades
+turn average into percentage
+if the value is > average, send to list, divide count of list on original grade average
+close file
+print output to the user
+
+
+"""
