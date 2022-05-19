4a9b8bc69c5586aa5bcf7625d23ab7d0247bfa42
mohammed talib <m.t.talib@hotmail.com>
2022-05-19 16:32:36 -0700

    finished pseudo-code

--- IS51Final.py
+++ IS51Final.py
@@ -10,3 +10,23 @@ print output to the user
 
 
 """
+
+"""
+def main 
+file=open file.txt
+grades=[]
+grades.append(int(line.strip))
+print="number of grades" len(grades)
+avg=0
+for grade in grades add avg to grades
+average /= len(grade)
+print average, avg
+
+def calculate_percent_above_average()
+count=0
+if grade > avg add to count
+return count*100/len(grades)
+print "percentage of grades above average"
+f.close
+
+"""
